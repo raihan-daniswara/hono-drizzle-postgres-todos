2ba70b9529d55d61c7767319164cba07a6e9b174
Raihan Daniswara <raihan.daniswara15@gmail.com>
2026-07-06 22:41:11 +0700

    feat: integrate schema into db initialization and implement get todos by id API endpoint with hono

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import * as schema from "./schema";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -7,4 +8,4 @@ export const pool = new Pool({
   idleTimeoutMillis: 30000,
 });
 
-export const db = drizzle(pool, { casing: "snake_case" });
+export const db = drizzle(pool, { schema, casing: "snake_case" });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { getTodosByUserId } from "./db/drizzle/queries";
+import { db } from "./db/db";
+import { UUID } from "crypto";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/todos", async (c) => {
+  const userId = c.req.query("userId");
+  if (!userId) {
+    return c.json({ error: "no user id provided" }, 400);
+  }
+  try {
+    const todos = await getTodosByUserId(db, userId as UUID);
+    return c.json({ todos }, 200);
+  } catch (error) {
+    console.log("Error fetching Todos: ", error);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
 
-export default app
+export default app;
